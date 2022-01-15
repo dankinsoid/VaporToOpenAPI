5ac14c9a70a60969fc817c817c7c51b66d0f8c0b
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2022-01-15 13:08:57 +0300

    hmm

--- Package.swift
+++ Package.swift
@@ -25,12 +25,6 @@ let package = Package(
 				dependencies: [
 					.product(name: "Vapor", package: "vapor"),
 					.product(name: "Swiftgger", package: "Swiftgger")
-				],
-				swiftSettings: [
-					// Enable better optimizations when building in Release configuration. Despite the use of
-					// the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-					// builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
-					.unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
 				]
 			),
 			.testTarget(
