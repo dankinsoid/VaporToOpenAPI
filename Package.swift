a11ed1877d2a57d6147947305e053104e0640a8c
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2023-03-11 15:25:20 +0400

    1.25.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
 		],
 		dependencies: [
 			// 💧 A server-side Swift web framework.
-			.package(url: "https://github.com/vapor/vapor.git", from: "4.70.0"),
-			.package(url: "https://github.com/dankinsoid/SwiftOpenAPI.git", from: "1.17.0"),
+			.package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+			.package(url: "https://github.com/dankinsoid/SwiftOpenAPI.git", from: "1.19.0"),
 		],
     targets: [
 			.target(
--- README.md
+++ README.md
@@ -84,7 +84,7 @@ import PackageDescription
 let package = Package(
   name: "SomeProject",
   dependencies: [
-    .package(url: "https://github.com/dankinsoid/VaporToOpenAPI.git", from: "1.24.0")
+    .package(url: "https://github.com/dankinsoid/VaporToOpenAPI.git", from: "1.25.0")
   ],
   targets: [
     .target(name: "SomeProject", dependencies: ["VaporToOpenAPI"])
